5cb10b8a019b8f1a416212cab77ca62ef005bd97
Frederik Riewerts <friewerts@pro-vision.de>
2019-08-23 10:07:51 +0200

    PVRND-498 | added prod script

--- packages/pv-scripts/bin/pv-scripts.js
+++ packages/pv-scripts/bin/pv-scripts.js
@@ -1,11 +1,40 @@
 #!/usr/bin/env node
 
-const getConfig = require('@pro-vision/webpack-config');
-const webpack = require('webpack');
-const webpackConfig = getConfig('production');
+const spawn = require('cross-spawn');
+const args = process.argv.slice(2);
 
-const compiler = webpack(webpackConfig);
-compiler.run((err, stats) => {
-  console.log('err', err);
-  console.log('st', stats);
-});
+const scriptIndex = args.findIndex(
+  x => x === 'prod' || x === 'dev'
+);
+
+const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
+
+const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
+
+
+if (scriptIndex !== -1) {
+  const result = spawn.sync(
+    'node',
+    nodeArgs
+      .concat(require.resolve('../scripts/' + script))
+      .concat(args.slice(scriptIndex + 1)),
+    { stdio: 'inherit' }
+  );
+  if (result.signal) {
+    if (result.signal === 'SIGKILL') {
+      console.log(
+        'The build failed because the process exited too early. ' +
+          'This probably means the system ran out of memory or someone called ' +
+          '`kill -9` on the process.'
+      );
+    } else if (result.signal === 'SIGTERM') {
+      console.log(
+        'The build failed because the process exited too early. ' +
+          'Someone might have called `kill` or `killall`, or the system could ' +
+          'be shutting down.'
+      );
+    }
+    process.exit(1);
+  }
+  process.exit(result.status);
+}
---
+++ packages/pv-scripts/scripts/prod.js
@@ -0,0 +1,75 @@
+const chalk = require('chalk');
+const webpack = require('webpack');
+const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
+const printBuildError = require('react-dev-utils/printBuildError');
+const getConfig = require('@pro-vision/webpack-config');
+
+const webpackConfig = getConfig('production');
+
+webpackBuild()
+  .then(
+    ({ stats, warnings }) => {
+      if (warnings.length) {
+        console.log(chalk.yellow('Compiled with warnings.\n'));
+        console.log(warnings.join('\n\n'));
+        console.log(
+          '\nSearch for the ' +
+            chalk.underline(chalk.yellow('keywords')) +
+            ' to learn more about each warning.'
+        );
+      } else {
+        console.log(chalk.green('Compiled successfully.\n'));
+      }
+    },
+    err => {
+      console.log(chalk.red('Failed to compile.\n'));
+      printBuildError(err);
+      
+      process.exit(1);
+    }
+  )
+  .catch(err => {
+    if (err && err.message) {
+      console.log(err.message);
+    }
+    process.exit(1);
+  });
+
+// Create the production build
+function webpackBuild() {
+
+  console.log('Creating an production build...');
+
+  const compiler = webpack(webpackConfig);
+  return new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      let messages;
+      if (err) {
+        if (!err.message) {
+          return reject(err);
+        }
+        messages = formatWebpackMessages({
+          errors: [err.message],
+          warnings: [],
+        });
+      } else {
+        messages = formatWebpackMessages(
+          stats.toJson({ all: false, warnings: true, errors: true })
+        );
+      }
+      if (messages.errors.length) {
+        // Only keep the first error. Others are often indicative
+        // of the same problem, but confuse the reader with noise.
+        if (messages.errors.length > 1) {
+          messages.errors.length = 1;
+        }
+        return reject(new Error(messages.errors.join('\n\n')));
+      }
+
+      return resolve({
+        stats,
+        warnings: messages.warnings,
+      });
+    });
+  });
+}

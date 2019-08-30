f6bc40f4e4434c741f4025d8a37caa0d226e2f44
Frederik Riewerts <frederik.riewerts@gmail.com>
2019-08-30 12:20:33 +0200

    assemble-lite chand imports to helper dir

--- packages/assemble-lite/assemble-lite.js
+++ packages/assemble-lite/assemble-lite.js
@@ -1,9 +1,9 @@
 const Handlebars = require('handlebars');
 
-const { loadTemplates } = require('./lib/template-helper');
-const { loadData } = require('./lib/data-helper');
-const { loadPartials } = require('./lib/partials-helper');
-const { assemblePages } = require('./lib/pages-helpers');
+const { loadTemplates } = require('./helper/template-helper');
+const { loadData } = require('./helper/data-helper');
+const { loadPartials } = require('./helper/partials-helper');
+const { assemblePages } = require('./helper/pages-helpers');
 
 const assemble = async (partialsGlob, pagesGlob, templatesGlob, dataGlob, target) => {
   const [templMap, data] = await Promise.all([

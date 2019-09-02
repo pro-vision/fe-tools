f05f2c967909bfd5e534a9656eb8c2653fd2eca3
Ingo Fahrentholz <ifahrentholz@pro-vision.de>
2019-09-02 13:25:03 +0200

    Fix bug when no hbsTarget is provided.

--- packages/webpack-config/src/helpers/paths.js
+++ packages/webpack-config/src/helpers/paths.js
@@ -68,18 +68,13 @@ export const appName = getAppName();
 /******************************************************************************
  ** CompileHTML helper
  ******************************************************************************/
-
+export const useHtmlCompiler = Boolean(config.hbsEntry && config.hbsTarget);
+export const hbsEntry = useHtmlCompiler ? resolveApp(config.hbsEntry) : "/";
+export const hbsTarget = useHtmlCompiler ? resolveApp(config.hbsTarget) : "/";
 // check if a hbs partial dir is provided
 export const hbsPartialDir = {
   partialDirs: config.hbsPartialDir ? [resolveApp(config.hbsPartialDir)] : []
 };
-
-const getRequiredHtmlCompilerConfig = () =>
-  Boolean(config.hbsEntry && config.hbsTarget);
-
-export const useHtmlCompiler = getRequiredHtmlCompilerConfig();
-export const hbsEntry = useHtmlCompiler ? resolveApp(config.hbsEntry) : "/";
-export const hbsTarget = resolveApp(config.hbsTarget);
 
 /******************************************************************************
  ** EOD CompileHTML helper

899e5181866fd7fe4e414d37606f5ef67352f506
Alexander Muthmann <amuthmann@pro-vision.de>
2019-09-10 11:32:02 +0200

    #15/#16: Fix review findings

--- examples/react-tsx/src/components/Hello.tsx
+++ examples/react-tsx/src/components/Hello.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
-interface HelloProps { compiler: string; framework: string }
+interface HelloProps {
+  compiler: string;
+  framework: string;
+}
 
 export const Hello: React.FunctionComponent<HelloProps> = (props: HelloProps) =>
   <h1>Hello from {props.compiler} and {props.framework}!</h1>;

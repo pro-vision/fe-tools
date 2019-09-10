1d2ad54c138708cb8bd511fcf1d5f5a95cd6d2b0
Frederik Riewerts <frederik.riewerts@gmail.com>
2019-09-10 15:35:17 +0200

    fixed stylemark async issue

--- packages/pv-stylemark/scripts/buildStylemarkLsg.js
+++ packages/pv-stylemark/scripts/buildStylemarkLsg.js
@@ -1,18 +1,18 @@
 const { copyStyleguideFiles } = require("../gulp-tasks/lsg_tasks/copyStyleguideFiles");
-const { assembleLSGComponents } = require("../gulp-tasks/lsg_tasks/assembleLSGComponents");
+const { assembleLSGComponents } = require("../gulp-tasks/assembleWrapper/assembleLSGComponents");
 const { buildStylemark } = require("../gulp-tasks/lsg_tasks/buildStylemark");
 const { copyClickdummyFiles } = require("../gulp-tasks/clickdummy_tasks/copyClickdummyFiles");
-const { assembleClickdummyComponents } = require("../gulp-tasks/clickdummy_tasks/assembleClickdummyComponents");
-const { assembleClickdummyPages } = require("../gulp-tasks/clickdummy_tasks/assembleClickdummyPages");
+const { assembleClickdummyComponents } = require("../gulp-tasks/assembleWrapper/assembleClickdummyComponents");
+const { assembleClickdummyPages } = require("../gulp-tasks/assembleWrapper/assembleClickdummyPages");
 
 const buildStylemarkLsg = async () => {
 
   await Promise.all([
     new Promise(resolve => copyStyleguideFiles(resolve)),
     new Promise(resolve => copyClickdummyFiles(resolve)),
-    assembleClickdummyComponents(),
-    assembleClickdummyPages(),
-    assembleLSGComponents()
+    new Promise(resolve => assembleClickdummyComponents(resolve)),
+    new Promise(resolve => assembleClickdummyPages(resolve)),
+    new Promise(resolve => assembleLSGComponents(resolve))
   ]);
   await new Promise(resolve => buildStylemark(resolve));
 };

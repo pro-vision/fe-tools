00bd86035b921663a08847bd703aedd50bdebbb1
mbehzad <mbehzad@pro-vision.de>
2020-03-26 23:07:16 +0100

    feat(webpack-config): add ".mjs" to webpack extentions to better support ootb esm modules
    webpack has .mjs in it's default resolve extentions
    (https://webpack.js.org/configuration/resolve/#resolveextensions) use it to utilize better tree
    shakeability of es modules which mostly come with the ".mjs" extention

--- packages/webpack-config/src/webpack/base/settings/resolve.js
+++ packages/webpack-config/src/webpack/base/settings/resolve.js
@@ -5,7 +5,7 @@ import { appPath } from "../../../helpers/paths";
 export const resolveSettings = {
   resolve: {
     // Add `.ts` as a resolvable extension.
-    extensions: [".ts", ".js", ".jsx", ".tsx"],
+    extensions: [".ts", ".mjs", ".js", ".jsx", ".tsx"],
     alias: {
       SRC: resolve(appPath, "src/"),
       JS: resolve(appPath, "src/js/"),

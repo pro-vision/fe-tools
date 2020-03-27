7640caed11255a0a0b98c4edaa698e7c970c65e6
mbehzad <mbehzad@pro-vision.de>
2020-03-27 22:01:15 +0100

    feat(webpack-config): load 3rd party dependencies and integrate in output source-map

--- packages/webpack-config/src/webpack/dev/module.js
+++ packages/webpack-config/src/webpack/dev/module.js
@@ -7,5 +7,6 @@ import { basicSettings } from "./settings/basicSettings";
 import { devServerSettings } from "./settings/devServerSettings";
 // Tasks
 import { moduleCompileCSS } from "./tasks/compileCSS/module";
+import { moduleLoadSourceMaps } from "./tasks/loadSourceMaps/module";
 
-export const devConfigModule = merge(defaultConfigModule, basicSettings, devServerSettings, moduleCompileCSS);
+export const devConfigModule = merge(defaultConfigModule, basicSettings, devServerSettings, moduleCompileCSS, moduleLoadSourceMaps);

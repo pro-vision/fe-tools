062c18bdb4372616b44c45fd9a9f86d6dd2d6326
Frederik Riewerts <frederik.riewerts@gmail.com>
2020-10-16 13:20:30 +0200

    build(pv-scripts): ie11 specific build-config for legacy build

--- packages/pv-scripts/webpack/base/settings/output/legacy.js
+++ packages/pv-scripts/webpack/base/settings/output/legacy.js
@@ -5,6 +5,22 @@ module.exports = {
     path: appTarget,
     publicPath,
     filename: "js/[name].legacy.js",
-    chunkFilename: "resources/js/chunks/[name].legacy.[chunkhash].js"
+    chunkFilename: "resources/js/chunks/[name].legacy.[chunkhash].js",
+    environment: {
+      // The environment supports arrow functions ('() => { ... }').
+      arrowFunction: false,
+      // The environment supports BigInt as literal (123n).
+      bigIntLiteral: false,
+      // The environment supports const and let for variable declarations.
+      const: true,
+      // The environment supports destructuring ('{ a, b } = obj').
+      destructuring: false,
+      // The environment supports an async import() function to import EcmaScript modules.
+      dynamicImport: false,
+      // The environment supports 'for of' iteration ('for (const x of array) { ... }').
+      forOf: false,
+      // The environment supports ECMAScript Module syntax to import ECMAScript modules (import ... from '...').
+      module: false
+    }
   }
 };

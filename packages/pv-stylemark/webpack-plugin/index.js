43b2485484c203682e37a01542d4cc76ea9fdf42
mbehzad <mbehzad@pro-vision.de>
2020-12-31 17:28:33 +0100

    perf(pv-stylemark): move adding files to watch list to the emit phase
    to prevent paths being searched for each entry, move the task to the emit phase which is only called
    once every compile iteration

--- packages/pv-stylemark/webpack-plugin/index.js
+++ packages/pv-stylemark/webpack-plugin/index.js
@@ -8,58 +8,58 @@ class PvStylemarkPlugin {
     this.watchedFiles = { staticStylemarkFiles: [], assembleFiles: [] };
   }
   apply(compiler) {
-    compiler.hooks.emit.tapPromise("PvStylemarkPlugin", (compilation) => {
-      const changedFiles = Array.from(compilation.fileTimestamps.keys()).filter(
-        (watchfile) => {
+    compiler.hooks.emit.tapAsync(
+      "PvStylemarkPlugin",
+      async (compilation, callback) => {
+        // add files for stylemark and assemble to webpack to watch
+        const filesToWatch = await getFilesToWatch();
+        const allFiles = Object.values(filesToWatch).flat();
+        if (Array.isArray(compilation.fileDependencies)) {
+          compilation.fileDependencies.push(...allFiles);
+        } else {
+          allFiles.forEach((file) => compilation.fileDependencies.add(file));
+        }
+
+        const changedFiles = Array.from(
+          compilation.fileTimestamps.keys()
+        ).filter((watchfile) => {
           return (
             (this.prevTimestamps.get(watchfile) || this.startTime) <
             (compilation.fileTimestamps.get(watchfile) || Infinity)
           );
-        }
-      );
-
-      const changedStylemarkFiles = changedFiles.filter((filePath) =>
-        this.watchedFiles.staticStylemarkFiles.includes(filePath)
-      );
-      const changedAssembleFiles = changedFiles.filter((filePath) =>
-        this.watchedFiles.assembleFiles.includes(filePath)
-      );
-
-      this.prevTimestamps = compilation.fileTimestamps;
-
-      // called during watch change, but the change wasn't in any relevant files
-      if (
-        changedFiles.length &&
-        !changedStylemarkFiles.length &&
-        !changedAssembleFiles.length
-      ) {
-        return Promise.resolve();
-      }
-
-      return buildStylemark({
-        // unless files were changed but none was a static stylemark file
-        shouldCopyStyleguideFiles:
-          !changedFiles.length || changedStylemarkFiles.length,
-        // unless files were changed but none was a assemble file
-        shouldAssemble: !changedFiles.length || changedAssembleFiles.length,
-      });
-    });
-
-    compiler.hooks.afterCompile.tapAsync(
-      "PvStylemarkPlugin",
-      (compilation, callback) => {
-        getFilesToWatch().then((filesToWatch) => {
-          const files = Object.values(filesToWatch).flat();
-          // memorize stylemark relevant files to compare during emit phase
-          this.watchedFiles = filesToWatch;
-
-          if (Array.isArray(compilation.fileDependencies)) {
-            compilation.fileDependencies.push(...files);
-          } else {
-            files.forEach((file) => compilation.fileDependencies.add(file));
-          }
-          callback();
         });
+
+        // memorize stylemark relevant files to compare during emit phase
+        this.watchedFiles = filesToWatch;
+
+        const changedStylemarkFiles = changedFiles.filter((filePath) =>
+          this.watchedFiles.staticStylemarkFiles.includes(filePath)
+        );
+        const changedAssembleFiles = changedFiles.filter((filePath) =>
+          this.watchedFiles.assembleFiles.includes(filePath)
+        );
+
+        this.prevTimestamps = compilation.fileTimestamps;
+
+        // called during watch change, but the change wasn't in any relevant files
+        if (
+          changedFiles.length &&
+          !changedStylemarkFiles.length &&
+          !changedAssembleFiles.length
+        ) {
+          callback();
+          return;
+        }
+
+        await buildStylemark({
+          // unless files were changed but none was a static stylemark file
+          shouldCopyStyleguideFiles:
+            !changedFiles.length || changedStylemarkFiles.length,
+          // unless files were changed but none was a assemble file
+          shouldAssemble: !changedFiles.length || changedAssembleFiles.length,
+        });
+
+        callback();
       }
     );
   }
